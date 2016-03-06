a8da9b96e780d34cfe96d80a65ea2c696962b39e
shiehjy <shiehjy@gmail.com>
2016-03-06 22:14:45 +0800

    add button

--- app/src/main/java/com/example/shiehjy/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/shiehjy/helloandroid/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        final TextView txvMsg=(TextView) findViewById(R.id.msg);
+        Button btnMsg=(Button) findViewById(R.id.btnMsg);
+        btnMsg.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                txvMsg.setText("Hi Android");
             }
         });
     }
