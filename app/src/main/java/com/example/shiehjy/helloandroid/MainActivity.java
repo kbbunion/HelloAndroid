b57934ec859b7195728e8be8b6c8b482538675fc
shiehjy <shiehjy@gmail.com>
2016-03-07 23:01:48 +0800

    add button

--- app/src/main/java/com/example/shiehjy/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/shiehjy/helloandroid/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.shiehjy.helloandroid;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView txvMsg=null;
     private Button btnMsg=null;
+    private EditText etxEmail=null;
+    private Button btnShowAlert=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +38,46 @@ public class MainActivity extends AppCompatActivity {
 
         txvMsg=(TextView) findViewById(R.id.msg);
         btnMsg=(Button) findViewById(R.id.btnMsg);
+        etxEmail=(EditText) findViewById(R.id.email);
+
         btnMsg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txvMsg.setText("Hi Android");
+                //txvMsg.setText("Hi Android");
+                txvMsg.setText(etxEmail.getText().toString());
+            }
+        });
+
+        btnShowAlert=(Button) findViewById(R.id.btnShowAlert);
+        btnShowAlert.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                AlertDialog.Builder alert=new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Warning");
+                alert.setMessage("are you confirm");
+                alert.setCancelable(false);
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        txvMsg.setText("Yes");
+                    }
+                });
+
+                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        txvMsg.setText("No");
+                    }
+                });
+
+                alert.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        txvMsg.setText("Cancel");
+                    }
+                });
+
+                alert.show();
             }
         });
     }
@@ -58,6 +99,14 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }
+
+        if(id==R.id.sub1){
+            txvMsg.setText("sub1");
+        }
+
+        if(id==R.id.sub2){
+            txvMsg.setText("sub2");
         }
 
         return super.onOptionsItemSelected(item);
